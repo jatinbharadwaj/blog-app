57eb1fccaa933b366c547c4eb30e7764e4771bdb
jatinbharadwaj <jitinrcks@gmail.com>
2021-01-07 16:10:27 +0530

    Post model added

---
+++ models/post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+var postSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Post", campgroundSchema);
